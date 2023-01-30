824d933871b21afdad426cdff9ba992a1808fbae
Tomazacre12 <shadow12122004@gmail.com>
2023-01-30 21:52:22 +0700

    them solidity bai 1

---
+++ firstcontract.sol
@@ -0,0 +1,10 @@
+pragma solidity >=0.7.0 <0.9.0;
+contract firstcontract {
+    uint saveData;
+    function set(uint x) public {
+        saveData = x;
+    }
+    function get() public view returns (uint x) {
+        return saveData;
+    }
+}
