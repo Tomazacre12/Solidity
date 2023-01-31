157b1afdb6490661fd3c751f44ad91a7e6822132
Tomazacre12 <shadow12122004@gmail.com>
2023-01-31 20:45:04 +0700

    them solidity bai 2

---
+++ Bai-02/firstcoin.sol
@@ -0,0 +1,25 @@
+pragma solidity >=0.7.0 <0.9.0;
+contract firstcoin {
+    address public minter;
+    mapping (address => uint) public balances;
+
+    event sent(address from, address to, uint amount);
+
+    constructor () {
+        minter = msg.sender;
+    }
+
+    function mint(address receiver, uint amount)public {
+        require(msg.sender == minter);
+        require(amount < 1e60);
+        balances[receiver] += amount;
+        // balances[receiver] = balances[receiver] + amount;
+    }
+
+    function send(address receiver, uint amount) public {
+        require(amount <= balances[msg.sender], "khong du tien ma doi chuyen");
+        balances[msg.sender] -= amount;
+        balances[receiver] += amount;
+        emit sent(msg.sender, receiver, amount);
+    }
+}
