531752f04615db7fc87230bdc428306aba4e64e5
Tomazacre12 <shadow12122004@gmail.com>
2023-02-04 11:14:49 +0700

    them solidity bai 6

--- Bai-06/auction.sol
+++ Bai-06/auction.sol
@@ -1,14 +1,23 @@
 pragma solidity >=0.7.0 <0.9.0;
 contract simpleAuction {
     // Variable
+    address payable public beneficiary;
     uint public auctionEndTime;
 
     uint public highestBid;
     address public highestBidder;
 
+    bool ended = false;
+
     mapping(address => uint) public pendingReturns;
 
     event highestBidIncrease(address bidder, uint amount);
+    event auctionEnded(address winner, uint amount);
+
+    constructor (uint _biddingTime, address payable _beneficiary){
+        beneficiary = _beneficiary;
+        auctionEndTime = block.timestamp + _biddingTime;
+    }
 
     // Function
     function bid() public payable {
@@ -28,8 +37,25 @@ contract simpleAuction {
     }
     function withdraw() public returns(bool) {
         uint amount = pendingReturns[msg.sender];
+        if (amount >0)  {
+            pendingReturns[msg.sender] = 0;
+            if(!payable(msg.sender).send(amount)){
+                pendingReturns[msg.sender] = amount;
+                return false;
+            }
+        }
+        return true;
     }
     function auctionEnd() public {
+        if (ended = false) {
+            revert("Auction has ended");
+        } 
+        if(block.timestamp < auctionEndTime) {
+            revert("Auction hasn't ended");
+        }
+        ended = true;
+        emit auctionEnded(highestBidder, highestBid);
 
+        beneficiary.transfer(highestBid);
     }
 }
